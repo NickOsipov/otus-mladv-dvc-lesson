df1b10483775015251e2e237629d75a5904de35c
NickOsipov <nick.osipov.91@gmail.com>
2025-05-26 21:25:48 +0300

    feat: dvc pipeline

--- tasks.py
+++ tasks.py
@@ -14,3 +14,4 @@ def dvc_setup(ctx):
     ctx.run("dvc remote default ${YC_BUCKET_NAME}")
     ctx.run("dvc remote modify --local ${YC_BUCKET_NAME} access_key_id ${YC_ACCESS_KEY}")
     ctx.run("dvc remote modify --local ${YC_BUCKET_NAME} secret_access_key ${YC_SECRET_KEY}")
+
